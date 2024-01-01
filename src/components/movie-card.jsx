354b6e842e911cdc71917c9b8b863409f8e1a30f
Vishesh Gupta <136686473+VG-05@users.noreply.github.com>
2024-01-02 00:50:55 +0400

    feat: bookmark button appearing

--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -1,10 +1,9 @@
-import "C:/Users/vishe/OneDrive - University of Waterloo/Desktop/movie watchlist app/src/App.css";
-
 export default function MovieCard({ movieName, posterUrl, bookmarked, setBookmarked }) {
     return (
-        <div className="movie-card">
-            <img src={posterUrl} alt={movieName} className="movie-poster" />
-            <div className="overlay"><button type="button" onClick={() => setBookmarked(!bookmarked)}>{bookmarked ? <i className="fa-solid fa-bookmark"></i> : <i className="fa-regular fa-bookmark"></i>}</button></div>
+        <div className="card text-bg-dark mb-3 d-inline-block" style={{ maxWidth: "345px" }}>
+            <img src={posterUrl} alt={movieName} className="card-img" />
+            <div className="card-img-overlay"><button className="btn btn-dark" type="button" onClick={() => setBookmarked(!bookmarked)}>{bookmarked ? <i className="fa-solid fa-bookmark"></i> : <i className="fa-regular fa-bookmark"></i>}</button></div>
         </div>
     )
 }
+
