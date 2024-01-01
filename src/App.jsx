246988101d56878432f21122f2f848f36f07c668
Vishesh Gupta <136686473+VG-05@users.noreply.github.com>
2024-01-02 00:25:18 +0400

    feat: added tmdb api functionsality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import MovieCard from "./components/movie-card"
 import Navbar from "./components/navbar"
 export default function App() {
   const [bookmarked, setBookmarked] = useState(false);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    let url = "https://api.themoviedb.org/3/discover/movie?api_key=d1c9e7f242991749c4f5c56cc8c8a7e8";
+    fetch(url).then(res => res.json())
+      .then(movies => setMovies(movies.results))
+      .catch(err => console.log(err))
+  })
+
   return (
     <>
       <Navbar />
-      <MovieCard movieName="Avengers"
-        posterUrl="C:\Users\vishe\OneDrive - University of Waterloo\Desktop\movie watchlist app\src\assets\example.jpeg"
-        bookmarked={bookmarked}
-        setBookmarked={setBookmarked} />
+      {movies.map((movie) => {
+        return (<MovieCard movieName={movie.title}
+          posterUrl={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          bookmarked={bookmarked}
+          setBookmarked={setBookmarked} />);
+      })}
 
     </>
   )
