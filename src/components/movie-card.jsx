9cd8edb84c17e821b95ed7946cd1a3786d0a9c53
Vishesh Gupta <136686473+VG-05@users.noreply.github.com>
2024-01-02 16:25:17 +0400

    style: bookmark button on hover

--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -1,9 +1,11 @@
+import "./movie-card.css";
+
 export default function MovieCard({ movieName, posterUrl, bookmarked, setBookmarked }) {
     return (
         <div className="col-lg-2 col-md-3 col-sm-4 col-6 p-0">
             <div className="card text-bg-dark border-0" id="movie-card">
                 <img src={posterUrl} alt={movieName} className="card-img rounded-0" />
-                <div className="card-img-overlay"><button className="btn btn-dark" type="button" onClick={() => setBookmarked(!bookmarked)}>{bookmarked ? <i className="fa-solid fa-bookmark"></i> : <i className="fa-regular fa-bookmark"></i>}</button></div>
+                <div className="card-img-overlay" id="bookmark-button"><button className="btn btn-dark" type="button" onClick={() => setBookmarked(!bookmarked)}>{bookmarked ? <i className="fa-solid fa-bookmark"></i> : <i className="fa-regular fa-bookmark"></i>}</button></div>
             </div>
         </div>
 
