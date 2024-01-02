a7de61084f27602c8a21d20b0c3416082d0f5eae
Vishesh Gupta <136686473+VG-05@users.noreply.github.com>
2024-01-02 18:08:25 +0400

    feat: Load More Button to load more movies from the TMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react"
 import MovieCard from "./components/movie-card"
 import Navbar from "./components/navbar"
+import "./App.css"
 export default function App() {
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  let url = `https://api.themoviedb.org/3/discover/movie?api_key=d1c9e7f242991749c4f5c56cc8c8a7e8&page=${currentPage}`;
   useEffect(() => {
-    let page = 1;
-    let url = `https://api.themoviedb.org/3/discover/movie?api_key=d1c9e7f242991749c4f5c56cc8c8a7e8&page=${page}`;
+
     fetch(url).then(res => res.json())
-      .then(movies => setMovies(movies.results))
-      .catch(err => console.log(err))
-  })
+      .then(data => setMovies(prevMovies => [...prevMovies, ...data.results]))
+      .catch(err => console.error("Error fetching data: ", err))
+  }, [url, currentPage])
+
+  function handleLoadMore() {
+    setCurrentPage(prevPage => prevPage + 1);
+  }
 
   return (
     <div className="bg-black">
@@ -21,6 +27,7 @@ export default function App() {
         })}
         </div>
       </div>
+      <button id="load-more-button" class="btn btn-outline-light" type="button" title="Load more movies" onClick={handleLoadMore}>Load More</button>
     </div>
   )
 }
