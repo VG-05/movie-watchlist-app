267286f122b2a4caa3ea063e94f5ab3cf4de9173
Vishesh Gupta <136686473+VG-05@users.noreply.github.com>
2024-01-02 20:52:37 +0400

    feat: Added localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,23 @@ import "./App.css"
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [bookmarked, setBookmarked] = useState(JSON.parse(localStorage.getItem("bookmarkedMovies")) || {});
+
   let url = `https://api.themoviedb.org/3/discover/movie?api_key=d1c9e7f242991749c4f5c56cc8c8a7e8&page=${currentPage}`;
   useEffect(() => {
 
     fetch(url).then(res => res.json())
-      .then(data => setMovies(prevMovies => [...prevMovies, ...data.results]))
+      .then(data => {
+        if (currentPage === 1) {
+          setMovies(data.results)
+        }
+        else {
+          setMovies(prevMovies => {
+            const uniqueNewMovies = data.results.filter(newMovie => !prevMovies.some(prevMovie => prevMovie.id === newMovie.id));
+            return [...prevMovies, ...uniqueNewMovies];
+          })
+        }
+      })
       .catch(err => console.error("Error fetching data: ", err))
   }, [url, currentPage])
 
@@ -17,18 +29,32 @@ export default function App() {
     setCurrentPage(prevPage => prevPage + 1);
   }
 
+  function toggleBookmark(movieID) {
+    setBookmarked(prevState => ({
+      ...prevState, ...{ [movieID]: !prevState[movieID] }
+    }))
+  }
+
+  useEffect(() => {
+    localStorage.setItem("bookmarkedMovies", JSON.stringify(bookmarked))
+  }, [bookmarked])
+
   return (
     <div className="bg-black">
       <Navbar />
       <div className="container-lg p-0 my-3">
-        <div className="row">{movies.map((movie) => {
-          return (<MovieCard movieName={movie.title}
-            posterUrl={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />);
-        })}
+        <div className="row">
+          {movies.map((movie) => {
+            return <MovieCard key={movie.id} movieName={movie.title}
+              posterUrl={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              isBookmarked={bookmarked[movie.id]}
+              toggleBookmark={() => toggleBookmark(movie.id)} />
+          }
+          )}
         </div>
       </div>
       <div className="d-flex justify-content-center">
-        <button class="btn btn-outline-light my-5" type="button" title="Load more movies" onClick={handleLoadMore}>Load More</button>
+        <button className="btn btn-outline-light my-5" type="button" title="Load more movies" onClick={handleLoadMore}>Load More</button>
       </div>
     </div>
   )
--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import "./movie-card.css";
 
-export default function MovieCard({ movieName, posterUrl }) {
-    const [bookmarked, setBookmarked] = useState(false);
+export default function MovieCard({ movieName, posterUrl, isBookmarked, toggleBookmark }) {
     return (
         <div className="col-lg-2 col-md-3 col-sm-4 col-6 p-0">
             <div className="card text-bg-dark border-0" id="movie-card">
                 <img src={posterUrl} alt={movieName} className="card-img rounded-0" />
-                <div className="card-img-overlay" id="bookmark-button"><button className="btn btn-dark" type="button" title="Save to watchlist" onClick={() => setBookmarked(!bookmarked)}>{bookmarked ? <i className="fa-solid fa-bookmark"></i> : <i className="fa-regular fa-bookmark"></i>}</button></div>
+                <div className="card-img-overlay" id="bookmark-button"><button className="btn btn-dark" type="button" title="Save to watchlist" onClick={toggleBookmark}>{isBookmarked ? <i className="fa-solid fa-bookmark"></i> : <i className="fa-regular fa-bookmark"></i>}</button></div>
             </div>
         </div>
 
