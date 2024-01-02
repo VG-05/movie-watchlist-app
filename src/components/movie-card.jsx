8c6cfe2640a7100d142571a071e45383fdb0730f
Vishesh Gupta <136686473+VG-05@users.noreply.github.com>
2024-01-02 16:41:20 +0400

    style: bookmark button title

--- src/components/movie-card.jsx
+++ src/components/movie-card.jsx
@@ -7,7 +7,7 @@ export default function MovieCard({ movieName, posterUrl }) {
         <div className="col-lg-2 col-md-3 col-sm-4 col-6 p-0">
             <div className="card text-bg-dark border-0" id="movie-card">
                 <img src={posterUrl} alt={movieName} className="card-img rounded-0" />
-                <div className="card-img-overlay" id="bookmark-button"><button className="btn btn-dark" type="button" onClick={() => setBookmarked(!bookmarked)}>{bookmarked ? <i className="fa-solid fa-bookmark"></i> : <i className="fa-regular fa-bookmark"></i>}</button></div>
+                <div className="card-img-overlay" id="bookmark-button"><button className="btn btn-dark" type="button" title="Save to watchlist" onClick={() => setBookmarked(!bookmarked)}>{bookmarked ? <i className="fa-solid fa-bookmark"></i> : <i className="fa-regular fa-bookmark"></i>}</button></div>
             </div>
         </div>
 
