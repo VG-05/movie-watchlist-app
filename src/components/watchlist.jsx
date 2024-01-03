6f967d28e00979c708d5f1711521353e849cb3b5
Vishesh Gupta <136686473+VG-05@users.noreply.github.com>
2024-01-04 01:27:48 +0400

    bugfix: fixed duplicating saved movies

--- src/components/watchlist.jsx
+++ src/components/watchlist.jsx
@@ -3,33 +3,39 @@ import Navbar from "./navbar";
 import MovieCard from "./movie-card";
 
 export default function Watchlist({ bookmarked, toggleBookmark }) {
-    const [movieDetails, setMovieDetails] = useState([]);
+    const [movieDetails, setMovieDetails] = useState(new Array());
     const apiKey = import.meta.env.VITE_API_KEY;
     let bookmarkedMovies = Object.entries(bookmarked).filter(([key, value]) => value === true);
     bookmarkedMovies = bookmarkedMovies.map(([key, value]) => parseInt(key));
     useEffect(() => bookmarkedMovies.forEach((movieID) => fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}`)
         .then(res => res.json())
         .then(data => setMovieDetails(prevState => {
-            let movies = [...prevState, data];
-            movies.filter(movie => !prevState.some(prevMovie => prevMovie.id === movie.id));
-            return movies;
+            const arr = [...prevState, data];
+            let uniqueArray = arr.filter((value, index) => {
+                const _value = JSON.stringify(value);
+                return index === arr.findIndex(obj => {
+                    return JSON.stringify(obj) === _value;
+                });
+            });
+            return uniqueArray;
         }))
         .catch(err => console.err("Error fetching data:", err))), []);
 
-    useEffect(() => console.log(movieDetails), [movieDetails]);
+
 
     return (
         <div className="bg-black">
             <Navbar />
             <div className="container-lg p-0 my-3">
                 <div className="row">
-                    {movieDetails.map(movie => {
-                        return <MovieCard key={movie.id} movieName={movie.title}
-                            posterUrl={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                            isBookmarked={bookmarked[movie.id]}
-                            toggleBookmark={() => toggleBookmark(movie.id)} />
-                    }
-                    )}
+                    {
+                        movieDetails.map(movie => {
+                            return <MovieCard key={movie.id} movieName={movie.title}
+                                posterUrl={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                isBookmarked={bookmarked[movie.id]}
+                                toggleBookmark={() => toggleBookmark(movie.id)} />
+                        }
+                        )}
                 </div>
             </div>
         </div>
